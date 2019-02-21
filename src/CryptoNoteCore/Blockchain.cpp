88dda9ed052838a5381df38e3ce2855a6e0c03e3
darkharvest <kahthan@gmail.com>
2019-02-21 08:54:07 +0530

    Update Blockchain.cpp

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -722,6 +722,12 @@ difficulty_type Blockchain::getDifficultyForNextBlock() {
   uint32_t block_index = m_blocks.size();
   uint8_t block_major_version = get_block_major_version_for_height(block_index + 1);
 
+  // manual difficulty reset
+  uint32_t blockIndex = block_index + 1;
+  if (blockIndex >= 185462 && blockIndex < 195760) {
+   return 160;
+  }
+
   if (block_major_version >= 4) {
     return m_currency.nextDifficultyLWMA3(timestamps, commulative_difficulties);
   } else {
@@ -972,6 +978,12 @@ difficulty_type Blockchain::get_next_difficulty_for_alternative_chain(const std:
 
   uint32_t block_index = m_blocks.size();
   uint8_t block_major_version = get_block_major_version_for_height(block_index + 1);
+
+  // manual difficulty reset
+  uint32_t blockIndex = block_index + 1;
+  if (blockIndex >= 185462 && blockIndex < 195760) {
+   return 160;
+  }
 
   if (block_major_version >= 4) {
     return m_currency.nextDifficultyLWMA3(timestamps, commulative_difficulties);
